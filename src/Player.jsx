5203b9ba62b723afa5346ea28bbd7eed3c926e09
christinamartinez <christina.martinez@faithlife.com>
2025-06-15 17:07:52 -0700

    Set default volume to 0.5

--- src/Player.jsx
+++ src/Player.jsx
@@ -22,7 +22,7 @@ const Player = ({ isPlaying: initialIsPlaying }) => {
     const defaultWaitBeforeGettingVolume = 3000; // 3 seconds
     const videoRef = useRef(null);
     const [isPlaying, setIsPlaying] = useState(initialIsPlaying);
-    const [volume, setVolume] = useState(1);
+    const [volume, setVolume] = useState(0.5);
     const [playbackSpeed, setPlaybackSpeed] = useState(1);
     const [socket, setSocket] = useState(null);
     const [gettingVolume, setGettingVolume] = useState(false);
